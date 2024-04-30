72bf17f15f1e9be3536aa460deef59a06fa878e2
Dionny Cárdenas <ds.cardenass@uniandes.edu.co>
2024-04-30 11:58:34 -0500

    Contenido Logica para el repositorio

---
+++ modern-art-museum/src/museum/museum.entity/museum.entity.ts
@@ -0,0 +1,31 @@
+import { ArtworkEntity } from 'src/artwork/artwork.entity/artwork.entity';
+import { ExhibitionEntity } from 'src/exhibition/exhibition.entity/exhibition.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class MuseumEntity {
+ @PrimaryGeneratedColumn('uuid')
+ id: string;
+
+ @Column()
+ name: string;
+ 
+ @Column()
+ description: string;
+ 
+ @Column()
+ address: string;
+ 
+ @Column()
+ city: string;
+
+ @Column()
+ image: string;
+
+ @OneToMany(() => ExhibitionEntity, exhibition => exhibition.museum)
+ exhibitions: ExhibitionEntity[];
+
+ @OneToMany(() => ArtworkEntity, artwork => artwork.museum)
+ artworks: ArtworkEntity[];
+}
+
